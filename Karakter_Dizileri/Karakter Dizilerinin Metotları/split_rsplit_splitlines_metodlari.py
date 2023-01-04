0df4a7e5624aeb7b853fed6ce3283e659d73a317
BatuhanYigit <batuhanyigit34@gmail.com>
2023-01-04 09:50:04 +0300

    split_update

--- Karakter_Dizileri/Karakter Dizilerinin Metotları/split_rsplit_splitlines_metodlari.py
+++ Karakter_Dizileri/Karakter Dizilerinin Metotları/split_rsplit_splitlines_metodlari.py
@@ -18,3 +18,13 @@ for i in deneme:
 
 for i in deneme.split("l"):
     print(i)
+
+split_deneme = "Ankara Büyükşehir Belediyesi"
+
+print(split_deneme.split(" "))
+
+#>> ['Ankara', 'Büyükşehir', 'Belediyesi']
+
+print(split_deneme.split(" ", 1)) #  1 sayısı sayesinde bölme işlemi karakter dizisi üzerine bir kez uygulandı. 
+
+#>> ['Ankara', 'Büyükşehir Belediyesi']
