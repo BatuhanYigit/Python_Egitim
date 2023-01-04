0dced73c5d0380b8bb3eda5dbe30f049228f152e
BatuhanYigit <batuhanyigit34@gmail.com>
2023-01-04 10:55:38 +0300

    update

---
+++ Karakter_Dizileri/Karakter Dizilerinin Metotları/lower_metodu.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+kisi = input("Aradığınız kişinin ismini giriniz : ")
+
+kisi = kisi.replace("I","ı").replace("İ","i").lower()
+
+if(kisi == "batuhan yiğit"):
+    print("email : batuhan.yigit@tga.gov.tr")
+    print("telephones : 05523875997")
+    print("address : istanbul")
+elif(kisi == "ahmet uçarkuş"):
+    print("email : ahmet.ucarkus@tga.gov.tr")
+    print("telephones : 05523875997")
+    print("address : Ankara")
+elif(kisi == "mertcan elik"):
+    print("email : mert.elik@tga.gov.tr")
+    print("telephones : 05523875997")
+    print("address : İzmir")
+else:
+    print("Böyle bir kişi yok")
