7c0d0c17419dade3fe5b0c454594190d7cf193b3
BatuhanYigit <batuhanyigit34@gmail.com>
2023-01-15 15:55:41 +0300

    update

---
+++ Temel Dosya İşlemleri/dosya_okumak.py
@@ -0,0 +1,3 @@
+ths = open("/home/batuhan/Documents/GitHub/Python_Egitim/Temel Dosya İşlemleri/deneme.txt")
+print(ths.read())
+ths.close()
---
+++ Temel Dosya İşlemleri/dosya_yazmak.py
@@ -0,0 +1,11 @@
+ths = open("/home/batuhan/Documents/GitHub/Python_Egitim/Temel Dosya İşlemleri/deneme.txt","w")
+ths.write("""
+Batuhan Yiğit
+Birsen Karakütük
+Deneme
+GÜlşen
+Nazlı
+Ahmet
+Mehmet
+ """)
+ths.close()
