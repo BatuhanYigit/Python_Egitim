08b340502eea3af2afc62b91d093dc13c5c251de
BatuhanYigit <batuhanyigit34@gmail.com>
2023-01-24 23:56:22 +0300

    update

--- Modüller/__doc__ Niteliği/doc_deneme.py
+++ Modüller/__doc__ Niteliği/doc_deneme.py
@@ -5,6 +5,4 @@ print(os.__doc__)
 """
 Bu komutları verdiğinizde, yukarıdaki karakter dizisinin çıktı olarak verildiğini göreceksiniz. Teknik dilde, üç tırnak içinde gösterilen karakter dizilerine belge dizisi (docstring) veya belgelendirme dizisi (documentation string) adı verilir. Modüllerin __doc__ niteliğini kullanarak, bir modül dosyasının en başında bulunan belgelendirme dizilerine erişebiliriz.
 
-
-
 """
