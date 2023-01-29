3a5791366dbeef91a0d41ce3f75466996c4cb613
BatuhanYigit <batuhanyigit34@gmail.com>
2023-01-29 19:52:33 +0300

    update

---
+++ Nesne Tabanlı Programlama (OOP)/Sınıf Üyeleri/Aleni Uyeler(Public Members)/public_members_ornek1.py
@@ -0,0 +1,13 @@
+class Sinif():
+    sinif_niteligi = "Sınıf Niteliği"
+
+    def ornek_metodu(self):
+        print("Ornek Metodu")
+    
+    @classmethod
+    def sinif_metodu(cls):
+        print("Sınıf Metodu")
+    
+    @staticmethod
+    def statik_metod():
+        print("Statik Metod")
