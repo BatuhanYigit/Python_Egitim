401d422bd195de2f9b8b37e73b35004857494803
BatuhanYigit <batuhanyigit34@gmail.com>
2023-02-02 16:50:33 +0300

    update

--- Pytest/integration_test _ornek/sqlquery.py
+++ Pytest/integration_test _ornek/sqlquery.py
@@ -2,9 +2,11 @@ create_table = """CREATE TABLE IF NOT EXISTS test (name, password, mail)"""
 
 check_data = """SELECT COUNT(*) FROM test"""
 
-insert_data = """INSERT INTO test VALUES ('{username}', '{password}', '{mail}')"""
+insert_data = """INSERT INTO test (name, password, mail) VALUES ('{username}', '{password}', '{mail}')"""
 
 list_data = """SELECT * FROM test"""
+
+delete_test = """DELETE FROM test WHERE id={delete_id}"""
 
 
 # information = {"username":username, "surname":surname, "phone":phone, "mail":mail}
--- Pytest/integration_test _ornek/test_main.py
+++ Pytest/integration_test _ornek/test_main.py
@@ -6,21 +6,24 @@ import sqlite3
 
 
 def test_df():
-    username = "dddd"
-    password = "asd"
+    username = "deneme"
+    password = "2222332"
     mail = "deneme"
     test.main(username,password,mail)
     conn = sqlite3.connect('/home/batuhan/Documents/GitHub/Python_Egitim/Pytest/integration_test _ornek/database.sqlite')
     sql_query = pd.read_sql_query(sqlquery.list_data,conn)
-
-    df = pd.DataFrame(sql_query, columns = ['name', 'password', 'mail'])
-
+    df = pd.DataFrame(sql_query, columns = ['id','name', 'password', 'mail'])
     last = df.iloc[-1]
     assert username == last['name']
     assert password == last['password']
     assert mail == last['mail']
+    delete_id = df.iloc[-1]['id']
+    id = {'delete_id':delete_id}
+    cur = conn.cursor()
+    cur.execute(sqlquery.delete_test.format(**id))
+    conn.commit()
+    
+    
+    
+    
 
-    print(df.iloc[-1])
-
-
-test_df()
